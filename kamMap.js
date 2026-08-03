74954a518de4fdc5c4944e01ea0d6afb1bff33d1
nguyen842-ai <nguyen842@gmail.com>
2026-08-03 15:38:39 +0700

    Create kamMap.js

---
+++ kamMap.js
@@ -0,0 +1,20 @@
+// =========================================================
+// BẢNG TỪ ĐIỂN ĐỔI TÊN KAM CŨ THÀNH KAM MỚI
+// Hướng dẫn: Viết HOA toàn bộ tên cũ bên trái, tên mới bên phải
+// =========================================================
+
+const kamMap = {
+"3. SOUTH MEKONG|TRẦN ĐÌNH PHÚC": "PHẠM CHIẾN THẮNG",
+"3. SOUTH MEKONG|PHẠM MINH SANG": "PHAN HỒ THANH TRÚC",
+"1. HCMC|ĐOÀN VĂN TUẤN": "CHÂU QUÍ",
+"2. NORTH MEKONG|PHAN VĂN SO": "HUỲNH VĂN GIÀU",
+"3. NORTH WEST|CDM TOẢN": "NGUYỄN NGỌC LONG",
+"1. HCMC|NGUYỄN THỊ KIỀU OANH": "NGÔ QUANG VINH",
+"3. SOUTH MEKONG|PHAN PHÚC SƠN": "NGUYỄN HỒNG KONG",
+"3. SOUTH MEKONG|DU MINH TÚ": "PHAN HỒ THANH TRÚC",
+"1. HCMC|TRẦN ANH KHÔI": "CHÂU QUÍ",
+"5. HLEC|NGUYỄN HOÀNG TRÂN": "HỒ VĂN TIẾN",
+"3. SOUTH MEKONG|HUỲNH VĂN TÍNH": "NGUYỄN TRUNG HIẾU",
+"2. NORTH COAST|CDM TOẢN": "NGUYỄN THANH QUỲNH",
+
+};
